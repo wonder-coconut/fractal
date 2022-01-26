6e75f55d8eebd6c50558e6470da93dccab69f59e
Rishab <rrishab2001@gmail.com>
2022-01-26 11:14:28 +0530

    weird fractal shit when passing number as string instead of integer

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const branches = document.getElementById('branches');
+const angle = document.getElementById('angle');
+const initialLine = document.getElementById('initial-line');
+const render = document.getElementById('render');
+const randomize = document.getElementById('randomize');
+
+const color1='white';
+const color2='white';
+
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function drawTree(x,y,angle,length,width,color1,color2)
+function drawTree(x,y,angle,angleDelta,length,width)
 {
     console.log(length);
     if(length < 10) return;
     const start = drawLine(x,y,angle,length,width,color1,color2);
     
-    drawTree(start.x, start.y,angle + 90, length * 0.75, width * 0.75, color1, color2);
-    drawTree(start.x, start.y,angle - 90, length * 0.75, width * 0.75, color1, color2);
+    drawTree(start.x, start.y,angle + angleDelta, angleDelta, length * 0.75, width * 0.75);
+    drawTree(start.x, start.y,angle - angleDelta, angleDelta, length * 0.75, width * 0.75);
 }
 
 function drawLine(x,y,angleDeg,length,width,color1,color2,)
@@ -30,4 +40,4 @@ function drawLine(x,y,angleDeg,length,width,color1,color2,)
     return {x: x + length * Math.cos(angleDeg * Math.PI/180),y: y - length * Math.sin(angleDeg * Math.PI/180)};
 }
 
-drawTree(canvas.width/2,canvas.height - 80, 90, 100, 10, 'white', 'white');
+drawTree(canvas.width/2,canvas.height - 80, 90, angle.value, 100, 10);

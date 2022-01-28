a854ca0e7189b1e9dd1c0546cbef968472a9f2ae
Rishab <rrishab2001@gmail.com>
2022-01-28 10:35:19 +0530

    input fields for all parameters (except branches)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const branches = document.getElementById('branches');
-const lengthReduction = document.getElementById('length-percent');
-const widthReduction = document.getElementById('width-percent');
 const angle = document.getElementById('angle');
+const angleDelta = document.getElementById('angle-delta');
+const len = document.getElementById('length');
+const width = document.getElementById('width');
+const lenChange = document.getElementById('length-percent');
+const widthChange = document.getElementById('width-percent');
 const initialLine = document.getElementById('initial-line');
 const render = document.getElementById('render');
 const randomize = document.getElementById('randomize');
@@ -11,54 +14,53 @@ const randomize = document.getElementById('randomize');
 const color1='white';
 const color2='white';
 
-let start;
+let start;//object with x and y coordinates
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
-initialLine.addEventListener('change', () => 
-{
-    console.log(initialLine.checked);
-});
-
 render.addEventListener('click', () =>
 {
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    //console.log(branches.value +', '+ widthReduction.value +', '+ lengthReduction.value +', '+ angle.value +', '+ initialLine.checked);
-    if((widthReduction.value <= 100 && widthReduction.value >= 0) && (lengthReduction.value <=100 && lengthReduction.value >= 0))
-        drawTree(canvas.width/2,canvas.height/2, 90, parseInt(angle.value), 100, 10,1);
+    if((widthChange.value <= 100 && widthChange.value >= 0) && (lenChange.value <=100 && lenChange.value >= 0))//percentage constraints
+        drawTree(canvas.width/2,canvas.height - 250, parseInt(angle.value), parseInt(len.value), parseInt(width.value),1);
     else
         console.log('out of bounds');
 });
 
 randomize.addEventListener('click', () => 
 {
+    branches.value = Math.round(Math.random()*5) + 1;
     angle.value = Math.round(Math.random()*360);
-    branches.value = Math.round(Math.random()*10);
-    lengthReduction.value = Math.round(Math.random()*100);
-    widthReduction.value = Math.round(Math.random()*100);
-
+    angleDelta.value = Math.round(Math.random()*360);
+    len.value = Math.round(Math.random()*390) + 10;
+    width.value = Math.round(Math.random()*39) + 1;
+    lenChange.value = Math.round(Math.random()*100);
+    widthChange.value = Math.round(Math.random()*100);
+    initialLine.checked = Math.round(Math.random());
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    drawTree(canvas.width/2,canvas.height/2, 90, parseInt(angle.value), 100, 10,1);
+    drawTree(canvas.width/2,canvas.height/2, 90, 100, 10,1);
 });
 
-function drawTree(x,y,angle,angleDelta,length,width,counter)
+function drawTree(x,y,angle,len,width,counter)
 {
-    if(length < 10) return;
+    if(len < 10) return;
     
     let start;
     if(counter == 1 && !initialLine.checked)
         start = {x: x,y: y};
     else
-        start = drawLine(x,y,angle,length,width,color1,color2);
+        start = drawLine(x,y,angle,len,width,color1,color2);
     
-    //console.log(length + ', '+start+', '+counter);
-    drawTree(start.x, start.y,angle + angleDelta, angleDelta, length * 0.75, width * 0.75, ++counter);
-    drawTree(start.x, start.y,angle - angleDelta, angleDelta, length * 0.75, width * 0.75, ++counter);
+    console.log(len + ', '+start+', '+counter);
+    len = len * parseInt(lenChange.value)/100;
+    width = width * parseInt(widthChange.value)/100;
+
+    drawTree(start.x, start.y,angle + parseInt(angleDelta.value),len,width,++counter);
+    drawTree(start.x, start.y,angle - parseInt(angleDelta.value),len,width,++counter);
 }
 
-function drawLine(x,y,angleDeg,length,width,color1,color2,)
+function drawLine(x,y,angleDeg,len,width,color1,color2,)
 {
     ctx.beginPath();
     ctx.save();
@@ -68,12 +70,11 @@ function drawLine(x,y,angleDeg,length,width,color1,color2,)
     ctx.translate(x,y);
     ctx.rotate(-angleDeg * Math.PI/180)
     ctx.moveTo(0,0);
-    ctx.lineTo(length, 0);
+    ctx.lineTo(len, 0);
     ctx.stroke();
     ctx.restore();
 
-    return {x: x + length * Math.cos(angleDeg * Math.PI/180),y: y - length * Math.sin(angleDeg * Math.PI/180)};
+    return {x: x + len * Math.cos(angleDeg * Math.PI/180),y: y - len * Math.sin(angleDeg * Math.PI/180)};
 }
 
-console.log()
-//TODO branches, width reduction, length reduction
+//TODO branches
